8f4807e3db356703638fdf2ed7c388678c7c69dc
aronmin <scm0418@naver.com>
2026-05-18 09:29:09 +0900

    First Commit

---
+++ src/main/java/com/finmeet/FinMeetApplication.java
@@ -0,0 +1,13 @@
+package com.finmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinMeetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinMeetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finmeet/FinMeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinMeetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
